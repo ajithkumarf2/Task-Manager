3bcd1bc3a7c756efd8683bdfccb36146136f753a
Ajithkumar E <ajithkumart601@gmail.com>
2026-07-07 09:47:12 +0530

    feat: initialize express server with middleware and core API routes

--- Server/server.js
+++ Server/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.status(200).send('Task Manager API');
+  res.status(200).send('Task Manager API is running');
 });
 
 // API Routing
